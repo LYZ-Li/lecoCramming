7ed16dc0709d584268091e2dd0937a70d0cb48df
LYZ-Li <liyuanzhemail@gmail.com>
2023-10-30 20:38:52 +0100

    try1

--- lyz/TwoSum.py
+++ lyz/TwoSum.py
@@ -1,1 +1,15 @@
-nums = [12,3,5,4]
+# 注意：python 代码由 chatGPT🤖 根据我的 java 代码翻译，旨在帮助不同背景的读者理解算法逻辑。
+# 本代码已经通过力扣的测试用例，应该可直接成功提交。
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 维护 val -> index 的映射
+        valToIndex = {}
+        for i in range(len(nums)):
+            # 查表，看看是否有能和 nums[i] 凑出 target 的元素
+            need = target - nums[i]
+            if need in valToIndex:
+                return [valToIndex[need], i]
+            # 存入 val -> index 的映射
+            valToIndex[nums[i]] = i
+        return []
