aedb0642899d523d019c256264da5ebdd61c6a3e
LYZ-Li <liyuanzhemail@gmail.com>
2023-11-05 14:24:06 +0100

    solved_ dict.get(key[,defaultValue]) Method

--- lyz/3.Longest Substring Without Repeating Characters.py
+++ lyz/3.Longest Substring Without Repeating Characters.py
@@ -8,6 +8,10 @@ class Solution:
             c = s[right]
             right += 1
             # 进行窗口内数据的一系列更新
+            # ? 这里的get方法是这个{}什么类型的变量的
+            # solved 05. Nov. 2023
+            # 这是一个hashmap 在python里叫dict。 get()方法是取得键值，
+            # get(key,default) 是在dict 中没有这个key的时候返回这个默认值，可选参数
             window[c] = window.get(c, 0) + 1
             # 判断左侧窗口是否要收缩
             while window[c] > 1:
